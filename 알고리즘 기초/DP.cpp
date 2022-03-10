2b4c094ff6d8bd866a248c3ea0945f44d7fb8493
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-03-10 15:16:44 +0900

    Update DP.cpp

--- 알고리즘 기초/DP.cpp
+++ 알고리즘 기초/DP.cpp
@@ -2,14 +2,29 @@
 
 using namespace std;
 
-int d(int x)
+//int d(int x)
+//{
+//	if (x == 1)return 1;
+//	if (x == 2)return 1;
+//	return d(x - 1) + d(x - 2);
+//}
+//
+//int main()
+//{
+//	printf("%d", d(50));
+//}
+
+int d[100];
+
+int fibonacci(int x)
 {
-	if (x == 1)return 1;
-	if (x == 2)return 1;
-	return d(x - 1) + d(x - 2);
+	if (x == 1) return 1;
+	if (x == 2) return 1;
+	if (d[x] != 0)return d[x];
+	return d[x] = fibonacci(x - 1) + fibonacci(x - 2);
 }
 
 int main()
 {
-	printf("%d", d(50));
+	printf("%d", fibonacci(30));
 }
