fb088607b08d34c2b2ee2628c58adf0ec0514aeb
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-03-10 15:16:45 +0900

    Create 11726.cpp

---
+++ 백준/11726.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+long d[1001];
+
+long dp(int n)
+{
+	if (n == 1) return 1;
+	if (n == 2) return 2;
+	if (d[n] != 0) return d[n];
+	return d[n]=(dp(n - 1) + dp(n - 2))%10007;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << dp(n)<<'\n';
+}
