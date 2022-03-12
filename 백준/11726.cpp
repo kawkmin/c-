24da1b6ec02acb80641889f53659af7406e0827b
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-03-13 00:00:49 +0900

    Update 11726.cpp

--- 백준/11726.cpp
+++ 백준/11726.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<iostream> 
 
 using namespace std;
 
@@ -7,7 +7,7 @@ long d[1001];
 long dp(int n)
 {
 	if (n == 1) return 1;
-	if (n == 2) return 2;
+	if (n == 2) return 2; 
 	if (d[n] != 0) return d[n];
 	return d[n]=(dp(n - 1) + dp(n - 2))%10007;
 }
