9e3d2ef13e2f275f7230a8cd8ac3d9516797d215
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-03-13 00:00:00 +0900

    Create 1009.cpp

---
+++ 백준/1009.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()		
+{
+	int n,a,b,q=0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+		for (int j = 0; j < b; j++) {
+			q = (q * a) % 10;
+		}
+		if (q == 0)
+			q = 10;
+		printf("%d\n", q);
+		q = 0;
+	}
+}
