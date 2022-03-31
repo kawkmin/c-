612aed31210df8905fa10901aea29ee953f3deac
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-03-31 14:32:30 +0900

    Update Binary search.cpp

--- 알고리즘 기초/Binary search.cpp
+++ 알고리즘 기초/Binary search.cpp
@@ -7,11 +7,15 @@ const int data1 = 7;
 
 int search(int start, int end, int target)
 {
-	if (start > end)return -1;
+	if (start > end)
+		return -1;
 	int mid = (start + end) / 2;
-	if (a[mid] == target)return mid;
-	else if (a[mid] > target)return search(start, mid - 1, target);
-	else return search(mid + 1, end, target);
+	if (a[mid] == target)
+		return mid;
+	else if (a[mid] > target)
+		return search(start, mid - 1, target);
+	else 
+		return search(mid + 1, end, target);
 }
 
 int main()
