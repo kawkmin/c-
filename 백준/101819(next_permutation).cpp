67b4ad680cba92a62a8b6a8f2a8ee2e4163517ee
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-05-09 17:25:49 +0900

    차이를 최대로(s2) next_permutation

---
+++ 백준/101819(next_permutation).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> v(N);
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+    int result = 0;
+    sort(v.begin(), v.end());
+    do {
+        int temp = 0;
+        for (int i = 0; i < v.size() - 1; i++)
+            temp += abs(v[i] - v[i + 1]);
+
+        result = max(result, temp);
+    } while (next_permutation(v.begin(), v.end()));
+    cout << result;
+
+}
