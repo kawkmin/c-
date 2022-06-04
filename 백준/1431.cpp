c187dc86373ba9b9ec79a0b690b74bac3d231ef9
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-06-04 17:02:25 +0900

    시리얼 번호(S3)

---
+++ 백준/1431.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int n;
+string a[1000];
+
+int integer_sum(string a) 
+{
+	int len = a.length();
+	int sum = 0;
+	for (int i = 0; i < len; i++) 
+	{
+		if (a[i] - '0' <= 9 && a[i] - '0' >= 0)
+			sum += a[i] - '0';
+	}
+	return sum;
+}
+
+bool compare(string a, string b)
+{
+	if (a.length() < b.length()) 
+		return 1;
+
+	else if (a.length() > b.length()) 
+		return 0;
+
+	else
+	{
+		int sum_a = integer_sum(a);
+		int sum_b = integer_sum(b);
+		if (sum_a < sum_b) 
+			return 1;		
+
+		else if (sum_a > sum_b) 
+			return 0;		
+
+		else 
+			return a < b;
+	}
+}
+
+int main() 
+{
+	cin >> n;
+	for (int i = 0; i < n; i++) 
+		cin >> a[i];
+
+	sort(a, a + n, compare);
+	for (int i = 0; i < n; i++) 
+		cout << a[i] << ' ' << endl;
+	
+}
