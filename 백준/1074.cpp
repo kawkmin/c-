090ac77c576a1ce0f8c216e105a089965471819a
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-08-10 22:57:01 +0900

    Z(S1) 분할정복 실패

---
+++ 백준/1074.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N, r, c;
+int result;
+
+void Z(int y, int x, int size)
+{
+    if (y == r && x == c)
+    {
+        cout << result;
+        return;
+    }
+
+    if (r < y + size && r >= y && c < x + size && c >= x)
+    {
+        Z(y, x, size / 2);
+        Z(y, x + size / 2, size / 2);
+        Z(y + size / 2, x, size / 2);
+        Z(y + size / 2, x + size / 2, size / 2);
+    }
+    else
+    {
+        result += size * size;
+    }
+}
+int main()
+{
+    cin >> N >> r >> c;
+    Z(0, 0, (int)pow(2,N));
+}
