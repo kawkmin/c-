8647da2d7c17a5e0c3791fcc6044207b53e7fd5c
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-08-11 23:02:18 +0900

    한 줄로 서기(S2)

---
+++ 백준/1138.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int N, left;
+    int ary[11] = { 0,};
+    cin >> N;
+    for (int i = 0; i < N; i++) 
+    {
+        cin >> left;
+        for (int j = 0; j < N; j++) 
+        {
+            if (left == 0 && ary[j] == 0) 
+            {
+                ary[j] = i + 1;
+                break;
+            }
+            else if (ary[j] == 0)
+            left--; 
+        }
+    }
+    for (int i = 0; i < N; i++) 
+    {
+        cout << ary[i] << " ";
+    }
+}
