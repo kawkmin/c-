59aca901ea8fa74b3136f63207fcb66764f9f729
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-08-18 16:27:02 +0900

    어린왕자(S3)

---
+++ 백준/1004.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n, x1, y1, x2, y2,nn, cnt, cx, cy, r;
+	int a, b, c;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) 
+	{
+		cnt = 0;
+		cin >> x1 >> y1 >> x2 >> y2 >> nn;
+		for (int j = 0; j < nn; j++) 
+		{
+			cin >> cx >> cy >> r;
+			a = (x1 - cx) * (x1 - cx) + (y1 - cy) * (y1 - cy);
+			b = (x2 - cx) * (x2 - cx) + (y2 - cy) * (y2 - cy);
+			c = r * r;
+			if (a < c && b < c)
+				continue;
+			else if (a < c || b < c)
+				cnt++;
+		}
+		cout << cnt << '\n';
+	}
+}
