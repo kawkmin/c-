ed151eef0869baaa61481ac838e4252de93f670a
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-08-21 19:20:57 +0900

    수열정리 (S4)

---
+++ 백준/1015.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, a[51], P[51],min=0;
+    cin >> N;
+    for (int i = 0; i < N; i++) 
+        cin >> a[i];
+    
+    for (int i = 0; i < N; i++) 
+    {
+        int m = 1001;
+        for (int j = 0; j < N; j++) 
+        {
+            if (a[j] < m) 
+            {
+                m = a[j];
+                min = j;
+            }
+        }
+        P[min] = i;
+        a[min] = 1001;
+    }
+    for (int i = 0; i < N; i++)
+        cout << P[i] << " ";
+   
+}
