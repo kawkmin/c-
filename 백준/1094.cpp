7006527c8258dc47311c732c016796a3be8c6747
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-08-25 23:39:30 +0900

    막대기(s5)

---
+++ 백준/1094.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int X;
+    cin >> X;
+
+    int cnt = 1, min = 64, sum = 64;
+    while (sum > X) 
+    {
+        min = min / 2;
+        if (sum - min >= X) 
+            sum -= min;       
+        else 
+            cnt++;     
+    }
+
+    cout << cnt;
+}
