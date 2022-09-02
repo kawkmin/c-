85cd9d6039563d04e753d5b5bbb266741730b7dd
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-02 16:46:51 +0900

    배열 합치기(S5) 분할정복

---
+++ 백준/11728.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int A[1000001], B[1000001];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+	int N, M,n=0,m=0;
+	cin >> N >> M;
+    for (int i = 0; i < N; i++) 
+        cin >> A[i];  
+    for (int i = 0; i < M; i++) 
+        cin >> B[i];
+    while (n < N && m < M) 
+    {
+        if (A[n] <= B[m])
+            cout << A[n++] << " ";
+        else
+            cout << B[m++] << " ";
+    }
+    while (n < N) 
+        cout << A[n++] << " ";
+    while (m < M)
+        cout << B[m++] << " ";
+}
