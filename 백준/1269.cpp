2496f51b86e43590b146ea4b5749254a6ae7cc8a
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-04 23:15:44 +0900

    대칭 차집합(S4) map

---
+++ 백준/1269.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include<map>
+using namespace std;
+
+map <int, int> m;
+int A, B, ans;
+int a[200001];
+int b[200001];
+int main() 
+{
+    cin >> A >> B;
+    for (int i = 0; i < A; i++) 
+        cin >> a[i];
+    for (int i = 0; i < B; i++)
+        cin >> b[i];
+
+    for (int i = 0; i < A; i++) 
+        m[a[i]]++;
+
+    for (int i = 0; i < B; i++) 
+        if (m[b[i]]) 
+            ans++;
+
+    cout << A + B - ans * 2;
+}
