fb33082b023e7d39efcbbc72bdf0b17bf6ee8b23
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-05 23:48:43 +0900

    올바른 배열(S4) distance

---
+++ 백준/1337.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int A[51];
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+
+    sort(A, A + N);
+    int ans = 4;
+    for (int i = 0; i < N; i++)
+    {
+        int tmp = distance(A + i, lower_bound(A, A + N, A[i] + 5));
+        ans = min(ans, 5 - tmp);
+    }
+
+    cout << ans;
+}
