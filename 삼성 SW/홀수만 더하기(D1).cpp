c5d5dc0c1b79beb91012de8dce8efd7bd7d2208c
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-07 14:31:37 +0900

    홀수만 더하기(D1)

---
+++ 삼성 SW/홀수만 더하기(D1).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int test_case;
+	int T;
+	cin >> T;
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int sum = 0;
+		int a;
+		for (int i = 0; i < 10; i++)
+		{
+			cin >> a;
+			if (a % 2 == 1)
+				sum += a;
+		}
+		cout <<"#"<<test_case<<" "<<sum<<"\n";
+	}
+	
+}
