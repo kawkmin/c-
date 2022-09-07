90a1edf6f67feca87668bf20c778e7f56bde976d
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-07 14:31:41 +0900

    Create 중간값 찾기(D1).cpp

---
+++ 삼성 SW/중간값 찾기(D1).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() 
+{
+	int test_case;
+	int T;
+	cin >> T;
+	int a[200];
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		cin >> a[test_case];
+	}
+	sort(a, a + T);
+	if (T % 2 == 0)
+		cout << a[T / 2 + 1];
+	else
+		cout << a[T / 2];
+}
