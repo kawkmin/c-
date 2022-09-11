b642fb3af207d8a13e3ea6051cbbf881c174afe1
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-11 13:09:54 +0900

    Create 이진 변환 반복하기(2).cpp

---
+++ 프로그래머스/이진 변환 반복하기(2).cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    int cnt1 = 0, cnt2 = 0;
+    while (1)
+    {
+        if (s == "1")
+            break;
+        cnt1++;
+        int cnt = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '0')
+                cnt++;
+        }
+        cnt2 += cnt;
+        int a = s.size() - cnt;
+        s = "";
+        while (a > 0)
+        {
+            s += to_string(a % 2);
+            a /= 2;
+        }
+    }
+    answer.push_back(cnt1);
+    answer.push_back(cnt2);
+    return answer;
+}
