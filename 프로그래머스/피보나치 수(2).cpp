ecc94a631c243552980633c3bd775907b069fec1
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-11 14:32:51 +0900

    Create 피보나치 수(2).cpp
    간단한 DP

---
+++ 프로그래머스/피보나치 수(2).cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+long long dp[100001];
+
+long long dy(int a) {
+    if (dp[a] || a == 0)
+        return dp[a];
+    else
+        return dp[a] = dy(a - 1) + dy(a - 2) % 1234567;
+}
+
+int solution(int n) {
+    long long answer = 0;
+    dp[0] = 0;
+    dp[1] = 1;
+    answer = (dy(n - 1) + dy(n - 2)) % 1234567;
+    return answer;
+}
