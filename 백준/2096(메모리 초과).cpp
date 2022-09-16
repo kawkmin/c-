e69b00c024b879f69251c9b6e1e3987acf2ef988
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-16 20:33:23 +0900

    내려가기(G5) dp 메모리 초과

---
+++ 백준/2096(메모리 초과).cpp
@@ -0,0 +1,56 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int N,a[100001][4];
+int dpmax[100001][4], dpmin[100001][4];
+
+int main()
+{
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= 3; j++)
+			cin >> a[i][j];
+
+	for (int i = 0; i <= N; i++)
+	{
+		for (int j = 0; j <= 3; j++)
+		{
+			dpmin[i][j] = 999999;
+		}
+	}
+	
+	for (int i = 1; i <= 3; i++)
+		dpmin[1][i] = a[1][i];
+
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= 3; j++)
+		{
+			dpmax[i][j] = max(dpmax[i - 1][j - 1], dpmax[i - 1][j]);
+			dpmax[i][j] = max(dpmax[i][j], dpmax[i - 1][j + 1]);
+			dpmax[i][j] += a[i][j];
+
+		}
+	}
+	for (int i = 2; i <= N; i++)
+	{
+		for (int j = 1; j <= 3; j++)
+		{
+			dpmin[i][j] = min(dpmin[i - 1][j - 1], dpmin[i - 1][j]);
+			dpmin[i][j] = min(dpmin[i][j], dpmin[i - 1][j + 1]);
+			dpmin[i][j] += a[i][j];
+		}
+	}
+	int MAX=0, MIN=999999;
+	for (int i = 1; i <= 3; i++)
+	{
+		if (MAX < dpmax[N][i])
+			MAX = dpmax[N][i];
+
+		if (MIN > dpmin[N][i])
+			MIN = dpmin[N][i];
+	}
+	cout << MAX<<" "<<MIN;
+
+}
