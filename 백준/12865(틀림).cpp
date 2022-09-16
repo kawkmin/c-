d1fa52cf312cdf95721350d6d375deb6ad2a84b3
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-16 16:53:34 +0900

    평범한 배낭(G5) DP 틀림

---
+++ 백준/12865(틀림).cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int dp[101][100001];
+
+int main()
+{
+	int N, K,W[101],V[101];
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> W[i] >> V[i];
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= K; j++)
+		{
+			if (j >= W[i])
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
+			else
+				dp[i][j] = dp[i - 1][j];
+		}
+	}
+	cout << dp[N][K];
+}
