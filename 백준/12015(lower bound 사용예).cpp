0a369286fdb40618747379e09a294c30ee89b17e
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-19 22:24:31 +0900

    가장 긴 증가하는 부분2(G2) lower_bound

---
+++ 백준/12015(lower bound 사용예).cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+
+int main()
+{
+	vector<int> v = { 1000001 };
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int a;
+		cin >> a;
+		if (a > v.back())
+			v.push_back(a);
+		else
+		{
+			v[lower_bound(v.begin(), v.end(), a) - v.begin()] = a;
+		}
+	}
+	cout << v.size();
+}
