06285335061c8fcb5950eee27debdfa9e084b9ef
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-23 20:41:34 +0900

    터렛(S3)

---
+++ 백준/1002.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x1, y1, r1, x2, y2, r2, d, a1, a2, num;
+    cin >> num;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+        d = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+        a1 = (r1 - r2) * (r1 - r2);
+        a2 = (r1 + r2) * (r1 + r2);
+        if (d == 0)
+        {
+            if (a1 == 0)
+                cout << "-1" << '\n';
+            else
+                cout << "0" << '\n';
+        }
+        else if (d == a1 || d == a2)
+            cout << "1" << '\n';
+        else if (a1 < d && d < a2)
+            cout << "2" << '\n';
+        else
+            cout << "0" << '\n';
+    }
+
+}
