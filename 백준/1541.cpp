ec7b8b5e7afafcc087cfb23939321ea33f0a1bd3
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-24 23:19:04 +0900

    잃어버린 괄호(S2)

---
+++ 백준/1541.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	int result = 0;
+	string num;
+	bool b = false;
+
+	for (int i = 0; i <= s.size(); i++)
+	{
+
+		if (s[i] == '-' || s[i] == '+' || i == s.size())
+		{
+			if (b)
+			{
+				result -= stoi(num);
+				num = "";
+			}
+			else
+			{
+				result += stoi(num);
+				num = "";
+			}
+		}
+		else
+			num += s[i];
+
+		if (s[i] == '-')
+			b = true;
+
+	}
+
+	cout << result;
+}
