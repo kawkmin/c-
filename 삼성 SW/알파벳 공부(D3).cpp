a880865c6bc5da466c76350bb68b2430b9037f44
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-28 12:01:30 +0900

    Create 알파벳 공부(D3).cpp

---
+++ 삼성 SW/알파벳 공부(D3).cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	int cnt = 0;
+	if (s[0] == 'a')
+	{
+		cnt++;
+		for (int i = 1; i < s.size(); i++)
+		{
+			if (s[i]==s[i-1]+1)
+				cnt++;
+			else
+				break;
+		}
+	}
+	cout << cnt;
+}
