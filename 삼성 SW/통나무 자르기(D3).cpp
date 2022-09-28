65d57698a29b176b77c65f96eb0d6865f05f2c89
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-28 13:16:19 +0900

    Create 통나무 자르기(D3).cpp

---
+++ 삼성 SW/통나무 자르기(D3).cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int test_case;
+    int T;
+    cin >> T;
+    for (test_case = 1; test_case <= T; ++test_case)
+    {
+        int a;
+        cin >> a;
+        if (a % 2 == 1)
+            cout << "#" << test_case << " " << "Bob" << "\n";
+        else
+            cout << "#" << test_case << " " << "Alice" << "\n";
+    }
+    return 0;
+}
