5a859fdd0d6589dc3e15ffb05ea62c9dc8d5cbc8
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-09-28 13:16:15 +0900

    Create 공과 잡초(D3).cpp

---
+++ 삼성 SW/공과 잡초(D3).cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int test_case;
+    int T;
+    cin >> T;
+    for (test_case = 1; test_case <= T; ++test_case)
+    {
+        int cnt = 0;
+        string s;
+        cin >> s;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(')
+            {
+                i++;
+                cnt++;
+            }
+            else if (s[i] == '|')
+                if (s[i + 1] == ')')
+                    cnt++;
+        }
+       cout << "#" << test_case << " " << cnt << "\n";
+    }
+    return 0;
+}
