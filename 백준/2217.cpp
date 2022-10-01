d9ea1f5c79e8e5ae1e9eac5c03fe8b90b2c340ea
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-01 16:41:17 +0900

    로프(S4)

---
+++ 백준/2217.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+long long a[100001];
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    sort(a, a + n);
+    long long MAX = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (MAX < a[i] * (n - i))
+            MAX = a[i] * (n - i);
+
+    }
+    cout << MAX;
+}
