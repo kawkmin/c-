620b56a664ab3a7da8105f0dbce6d6680b8b512c
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-02 17:08:21 +0900

    Create Flatten(D3).cpp

---
+++ 삼성 SW/Flatten(D3).cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int test_case;
+    for (test_case = 1; test_case <= 10; ++test_case)
+    {
+        int dump;
+        cin >> dump;
+        int box[101];
+        for (int i = 0; i < 100; i++)
+        {
+            cin >> box[i];
+        }
+        for (int i = 0; i < dump; i++)
+        {
+            sort(box, box + 100);
+            box[0]++;
+            box[99]--;
+        }
+        sort(box, box + 100);
+        cout << "#" << test_case << " " << box[99] - box[0] << "\n";
+    }
+}
