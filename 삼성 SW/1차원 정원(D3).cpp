d0b6c29b45bfddb0c43be06bd196ccf5cb7bbbc5
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-02 15:09:22 +0900

    Create 1차원 정원(D3).cpp

---
+++ 삼성 SW/1차원 정원(D3).cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int test_case;
+	int T;
+	cin >> T;
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int N, D;
+		cin >> N >> D;
+		int x = D * 2 + 1;
+		int cnt = N / x;
+		if (N % x)
+			cnt++;
+		cout << "#" << test_case << " "<<cnt << "\n";
+	}
+	return 0;
+}
