8d2762c768f4aab3e0d00f3216ad064d1dfffa8b
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-05 15:19:16 +0900

    랜선 자르기(S2) 이분 탐색 다시

---
+++ 백준/1654다시품.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+int a[10001];
+
+int main()
+{
+	int K, N;
+	cin >> K >> N;
+	for (int i = 0; i < K; i++)
+		cin >> a[i];
+	sort(a, a + K);
+	long long start = 1;
+	long long end = a[K - 1];
+	long long mid,result;
+
+	while (start <= end)
+	{
+		mid = (start + end) / 2;
+		int sum = 0;
+		for (int i = 0; i < K; i++)
+		{
+			sum += a[i] / mid;
+		}
+		if (sum >= N)
+		{
+			result = mid;
+			start = mid + 1;
+		}
+		else
+			end = mid - 1;
+	}
+	cout << result;
+}
