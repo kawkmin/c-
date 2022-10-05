aebb02f7c188743cef908d49aa143f642cedf597
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-05 17:10:46 +0900

    기타 레슨(S1) 이분 탐색 실패

---
+++ 백준/2343.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+
+using namespace std;
+long long a[100001];
+
+int main()
+{
+	long long N, M, sum = 0;
+	cin >> N >> M;
+	long long start = -1;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> a[i];
+		sum += a[i];
+		start = max(start, a[i]);
+	}
+	long long end = sum;
+	while (start <= end)
+	{
+		long long cnt = 0;
+		long long tempsum = 0;
+		long long mid = (start + end) / 2;
+		for (int i = 0; i < N; i++)
+		{
+			if (tempsum + a[i] > mid)
+			{
+				cnt++;
+				tempsum = 0;
+			}
+			tempsum += a[i];
+		}
+		if (tempsum != 0) cnt++;
+		if (cnt <= M)
+		{
+			end = mid - 1;
+		}
+		else
+			start = mid + 1;
+	}
+	cout << start;
+}
