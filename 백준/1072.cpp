d13465a92da0c1b8db05364f869c77bca962b8ab
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-06 15:30:55 +0900

    게임(S3) 이분 탐색

---
+++ 백준/1072.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	long long X, Y;
+	cin >> X >> Y;
+	long long Z = (100 * Y) / X;
+	long long start = 1;
+	long long end = X;
+	bool b = false;
+	long long mid;
+	while (start <= end)
+	{
+		long long x = X, y = Y;
+		mid = (start + end) / 2;
+		x += mid;
+		y += mid;
+		long long z = (y * 100) / x;
+		if (z > Z)
+		{
+			b = true;
+			end = mid - 1;
+		}
+		else
+			start = mid + 1;
+	}
+	if (!b)
+		cout << -1;
+	else
+		cout << start;
+}
