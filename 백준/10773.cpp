b15c47305207e8c6eb612f2d8fd78a45d2827b7f
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-07 23:25:01 +0900

    재로(S4) stack

---
+++ 백준/10773.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() 
+{
+    int K, n;
+    cin >> K;
+    stack<int> s;
+    for (int i = 0; i < K; i++)
+    {
+        cin >> n;
+        if (n != 0) 
+            s.push(n);
+        else 
+            s.pop();
+    }
+    long long sum = 0;
+    int size = s.size();
+    for (int i = 0; i < size; i++) 
+    {
+        sum += s.top();
+        s.pop();
+    }
+    cout << sum;
+}
