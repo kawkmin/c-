a8a8f0d9eb0971928c1cb257c9dfa750226acfb8
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-08 19:53:53 +0900

    알파벳 (G4) DFS

---
+++ 백준/1987 다시 품(똑같네..).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+int R, C;
+char board[21][21];
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
+int alpha[27];
+int cnt = 0;
+
+void dfs(int y, int x,int dep)
+{
+	cnt = max(cnt, dep);
+	for (int i = 0; i < 4; i++)
+	{
+		int ax = x + dx[i];
+		int ay = y + dy[i];
+		if (ax >= 0 && ay >= 0 && ax < C && ay < R && !alpha[board[ay][ax] - 'A'])
+		{
+			alpha[board[ay][ax] - 'A'] = 1;
+			dfs(ay, ax, dep + 1);
+			alpha[board[ay][ax] - 'A'] = 0;
+		}
+	}
+}
+
+int main() 
+{
+	cin >> R >> C;
+	for (int i = 0; i < R; i++)
+		for (int j = 0; j < C; j++)
+			cin >> board[i][j];
+	alpha[board[0][0] - 'A'] = 1;
+	dfs(0, 0, 1);
+	cout << cnt;
+}
