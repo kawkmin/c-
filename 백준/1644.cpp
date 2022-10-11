03c0c9984b1ef763c0ffbaff140ca581d20edbbd
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-11 15:47:39 +0900

    소수의 연속합(G3) 에라토스+DFS(이분탐색도 가능)

---
+++ 백준/1644.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+int a[4000001],temp[4000001];
+int N,cnt=0;
+int index = 0;
+
+void dfs(int x,int dep)
+{
+	if (dep >= index)
+		return;
+	x += temp[dep];
+	if (x == N)
+	{
+		cnt++;
+		return;
+	}
+	if (x > N)
+		return;
+	dfs(x , dep +1);
+}
+
+int main()
+{
+	cin >> N;
+	for (int i = 2; i <= N; i++)
+		a[i] = i;
+	for (int i = 2; i <= N; i++)
+	{
+		if (a[i] == 0) 
+			continue;
+		for (int j = i + i; j <=N; j += i)
+		{
+			a[j] = 0;
+		}
+	}
+	for (int i = 2; i <=N; i++)
+	{
+		if (a[i])
+		{
+			temp[index] = a[i];
+			index++;
+		}
+	}
+
+	for (int i = 0; i < index; i++)
+		dfs(0, i);
+	cout << cnt;
+}
