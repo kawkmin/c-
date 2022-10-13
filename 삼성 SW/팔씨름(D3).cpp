e826442a1ae6f69178436f1a77f59e79d951d810
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-13 15:07:04 +0900

    Create 팔씨름(D3).cpp

---
+++ 삼성 SW/팔씨름(D3).cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int test_case;
+	int T;
+	cin >> T;
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+        string s;
+        cin >> s;
+        int cnt = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == 'x')
+                cnt++;
+        }
+        cout << s << "\n";
+        if (cnt <= 7)
+            cout << "#" << test_case << " YES" << "\n";
+        else
+            cout << "#" << test_case << " NO" << "\n";
+
+	}
+	return 0;
+}
