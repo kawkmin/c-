bda98405689e8b68fa7e886dd4b0463562dbc32c
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-15 14:47:04 +0900

    Create 숫자 조작(D3) swap string.cpp

---
+++ 삼성 SW/숫자 조작(D3) swap string.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
+    cin >> T;
+    for (test_case = 1; test_case <= T; ++test_case)
+    {
+        string s;
+        cin >> s;
+        string MAX = s;
+        string MIN = s;
+        for (int i = 0; i < s.size() - 1; i++)
+        {
+            for (int j = i + 1; j < s.size(); j++)
+            {
+                string temp = s;
+                swap(temp[i], temp[j]);
+                if (temp[0] != '0')
+                {
+                    MAX = max(temp, MAX);
+                    MIN = min(temp, MIN);
+                }
+            }
+        }
+        cout << "#" << test_case << " " << MIN << " " << MAX << "\n";
+    }
+    return 0;
+}
