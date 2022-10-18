2ee3374f55a67fd5f7b24753aea7a051ab2f3ea5
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-18 17:16:43 +0900

    상자넣기 (S2) DP Lis

---
+++ 백준/1965(틀림).cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,box[1001],dp[1001],result=0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> box[i];
+	
+	for (int i = 0; i < n; i++)
+	{
+		dp[i] = 1;
+		for (int j = 0; j <i; j++)
+		{
+			if (box[i] > box[j])
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+		result = max(result, dp[i]);
+	}
+	cout << result;
+}
