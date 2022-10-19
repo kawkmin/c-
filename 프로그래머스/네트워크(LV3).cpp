4027e3930dfd274af507ec934e113050ce9e9f9d
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-19 15:51:56 +0900

    네트워크(LV3) BFS

---
+++ 프로그래머스/네트워크(LV3).cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+#include<queue>
+using namespace std;
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    int visit[201] = { 0, };// 프로그래머스는 이거해야함 안해서 1시간날림
+    queue<int> q;
+    for (int t = 0; t < n; t++)
+    {
+        if (!visit[t])
+        {
+            visit[t] = 1;
+            q.push(t);
+            answer += 1;
+        }
+        while (!q.empty())
+        {
+            int x = q.front();
+            q.pop();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!visit[i] && computers[x][i] == 1)
+                {
+                    q.push(i);
+                    visit[i] = 1;
+                }
+            }
+        }
+    }
+    return answer;
+}
