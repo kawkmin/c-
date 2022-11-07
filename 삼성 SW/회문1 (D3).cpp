df38635f1383806f68fbba7b4a467d15b8d9485c
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-07 17:34:08 +0900

    Create 회문1 (D3).cpp

---
+++ 삼성 SW/회문1 (D3).cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+#include<cmath>
+#include<cstring>
+using namespace std;
+string s[10];
+int N,ans;
+int visit[10] = { 0, };
+
+bool valid(string s)
+{
+	for (int i = 0; i < s.length()/2; i++)
+	{
+		if (s[i] == s[s.length()-1-i]) 
+			continue;
+		else 
+			return false;
+	}
+	return true;
+}
+
+int main(int argc, char** argv)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int test_case;
+	for (test_case = 1; test_case <= 10; ++test_case)
+	{
+		ans = 0;
+		cin >> N;
+		for (int i = 0; i < 8; i++)
+			cin >> s[i];
+		for (int t = 0; t < 8; t++)
+		{
+			for (int i = 0; i < 8 - N+1; i++)
+			{
+				string str = "";
+				string str2 = "";
+				for (int j = i; j < i + N; j++)
+				{
+					str += s[t][j];
+					str2 += s[j][t];
+				}
+				if (valid(str))
+					ans++;
+				if (valid(str2))
+					ans++;
+			}
+		}
+		cout << "#" << test_case <<" "<<ans<< "\n";
+	}
+	return 0;
+}
+
