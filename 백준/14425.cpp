7f557313ed1d8df2c529f2f0d1f47e51b79cf333
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-08 20:34:32 +0900

    문자열 집합(S3) MAP

---
+++ 백준/14425.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<string>
+#include<map>
+using namespace std;
+map<string,int> m;
+
+int main()
+{
+	int N,M,ans=0;
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++)
+	{
+		string s;
+		cin >> s;
+		m.insert({ s, i });
+	}
+	for (int i = 0; i < M; i++)
+	{
+		string str2;
+		cin >> str2;
+		if (m[str2])
+			ans++;
+	}
+	cout << ans;
+}
+
