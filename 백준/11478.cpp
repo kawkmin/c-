07579e452c27ef73b8c05c55e857dc62b4014b04
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-08 21:19:53 +0900

    서로 다른 부분 문자열의 개수(S3) map

---
+++ 백준/11478.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<map>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	vector<char> v;
+	map<string,int> m;
+	string s;
+	int ans = 0;
+	cin >> s;
+	for (int i = 0; i < s.length(); i++)
+	{
+		v.push_back(s[i]);
+	}
+	for (int i = 0; i < v.size(); i++)
+	{
+		string s = "";
+		for (int j = i; j < v.size(); j++)
+		{
+			s += v[j];
+			if (m[s]) continue;
+			m[s] = 1;
+			ans++;
+		}
+	}
+	cout << ans;
+}
+
