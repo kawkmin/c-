12e66063ed81a452f883c47bd05a45456b1aeb6f
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-09 21:02:17 +0900

    패션왕 신해빈(s3) map

---
+++ 백준/9375.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<map>
+using namespace std;
+
+int main()
+{
+	int test_case;
+	int T;
+	cin >> T;
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int n;
+		cin >> n;
+		string s;
+		string s2;
+		map<string, int> m;
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> s>>s2;
+			m[s2]++;
+		}
+		int ans = 1;
+		
+		for (auto a : m)
+		{
+			ans *= (a.second+1);
+		}
+		cout << ans - 1<<"\n";
+	}
+}
+
