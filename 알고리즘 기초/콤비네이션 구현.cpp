b34c0e5588792f67abc60240d43de979b970b543
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-12 14:35:22 +0900

    Create 콤비네이션 구현.cpp

---
+++ 알고리즘 기초/콤비네이션 구현.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<map>
+
+using namespace std;
+
+
+int main()
+{
+    string s = "ABCD";
+    map<string, int> m;
+
+    do {
+        vector<char> v;
+        for (int i = 0; i < s.size(); i++)
+        {
+            v.push_back(s[i]);
+            string temp = "";
+            for (int j = 0; j < v.size(); j++)
+                temp += v[j];
+            sort(temp.begin(), temp.end());
+            if (!m[temp])
+            {
+                m[temp] = 1;
+                cout << temp << "\n";
+            }
+        }
+    } while (next_permutation(s.begin(), s.end()));
+}
