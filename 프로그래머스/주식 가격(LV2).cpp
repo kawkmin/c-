3fdda3dac29dc7b7bd63eb0fad0efd8115499c00
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-15 19:51:06 +0900

    Create 주식 가격(LV2).cpp

---
+++ 프로그래머스/주식 가격(LV2).cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#include<queue>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size());
+
+    for (int i = 0; i < prices.size(); i++)
+    {
+        for (int j = i + 1; j < prices.size(); j++)
+        {
+            answer[i]++;
+            if (prices[j] < prices[i]) break;
+        }
+    }
+    return answer;
+}
