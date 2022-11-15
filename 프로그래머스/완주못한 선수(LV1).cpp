1a17ab44859981253067548d5b2fbb9136be8506
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-15 14:58:34 +0900

    Create 완주못한 선수(LV1).cpp

---
+++ 프로그래머스/완주못한 선수(LV1).cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include<map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    map<string, int> m;
+    for (int i = 0; i < participant.size(); i++)
+    {
+        if (!m[participant[i]])
+            m[participant[i]] = 1;
+        else
+            m[participant[i]]++;
+    }
+    for (int i = 0; i < completion.size(); i++)
+    {
+        m[completion[i]]--;
+    }
+    for (auto a : m) {
+        if (a.second >= 1)
+        {
+            for (int i = 0; i < a.second; i++)
+                answer += a.first;
+        }
+    }
+    return answer;
+}
