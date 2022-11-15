397b4309aadb199fa4af78a0184f6cb2198077e6
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-15 14:24:17 +0900

    Create 푸드 파이트 대회(LV1).cpp

---
+++ 프로그래머스/푸드 파이트 대회(LV1).cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    string temp = "";
+    for (int i = 1; i < food.size(); i++)
+    {
+        int x = food[i] / 2;
+        for (int j = 0; j < x; j++)
+            temp += to_string(i);
+    }
+    answer += temp + "0";
+    for (int i = temp.size() - 1; i >= 0; i--)
+        answer += temp[i];
+    return answer;
+}
