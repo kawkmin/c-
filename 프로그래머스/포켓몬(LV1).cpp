f27edfeff853975fa68f3931fe3c130a0ac38013
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-15 14:04:53 +0900

    Create 포켓몬(LV1).cpp

---
+++ 프로그래머스/포켓몬(LV1).cpp
@@ -0,0 +1,19 @@
+﻿#include <vector>
+#include<map>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    map<int, int> m;
+    int answer = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (!m[nums[i]]) {
+            m[nums[i]] = 1;
+            answer++;
+        }
+        if (answer == nums.size() / 2)
+            break;
+    }
+    return answer;
+}
