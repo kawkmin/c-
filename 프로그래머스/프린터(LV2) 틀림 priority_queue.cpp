4c48e8dd704befcd10e771b0271f6ca7a6846408
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-16 15:03:30 +0900

    Create 프린터(LV2) 틀림 priority_queue.cpp

---
+++ 프로그래머스/프린터(LV2) 틀림 priority_queue.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include<queue>
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    queue<pair<int, int>> q;
+    priority_queue<int> q2;
+    for (int i = 0; i < priorities.size(); i++)
+    {
+        q.push({ i,priorities[i] });
+        q2.push(priorities[i]);
+    }
+    while (!q.empty())
+    {
+        int index = q.front().first;
+        int value = q.front().second;
+        q.pop();
+        if (value == q2.top())
+        {
+            q2.pop();
+            answer++;
+            if (index == location)
+                break;
+        }
+        else
+            q.push({ index,value });
+    }
+
+    return answer;
+}
