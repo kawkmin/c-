92dfe98f6d5d9c4a6d4f8c5ddff4cbc4a0bde0bd
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-17 16:19:34 +0900

    Create 올바른 괄호(LV2).cpp

---
+++ 프로그래머스/올바른 괄호(LV2).cpp
@@ -0,0 +1,25 @@
+﻿#include<string>
+#include <iostream>
+#include<stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<int> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(')
+            st.push(1);
+        else
+        {
+            if (st.empty())
+                return false;
+            st.pop();
+        }
+    }
+    if (!st.empty())
+        return false;
+    return answer;
+}
