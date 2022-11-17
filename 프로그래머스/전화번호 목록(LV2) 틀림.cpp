4ae34de9214725a590e850ef940cc5df2d5b5f45
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-17 18:01:02 +0900

    Create 전화번호 목록(LV2) 틀림.cpp

---
+++ 프로그래머스/전화번호 목록(LV2) 틀림.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    map<string, int> map1;
+    for (int i = 0; i < phone_book.size(); i++)
+        map1[phone_book[i]] = 1;
+
+    for (int i = 0; i < phone_book.size(); i++)
+    {
+        for (int j = 0; j < phone_book[i].size() - 1; j++)
+        {
+            string phone_number = phone_book[i].substr(0, j + 1);
+            if (map1[phone_number])
+                return false;
+        }
+    }
+    return true;
+}
