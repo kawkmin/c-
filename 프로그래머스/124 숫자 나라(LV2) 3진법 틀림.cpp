bfad67cd95baf3c9546444ca061d67772ec60b1b
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-17 15:00:06 +0900

    Create 124 숫자 나라(LV2) 3진법 틀림.cpp

---
+++ 프로그래머스/124 숫자 나라(LV2) 3진법 틀림.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+string solution(int n) {
+    char array[3] = { '4','1','2' };
+    string answer = "";
+    while (n)
+    {
+        answer = array[n % 3] + answer;
+        if (n % 3 == 0)
+            n = n / 3 - 1;
+        else
+            n /= 3;
+    }
+    return answer;
+}
