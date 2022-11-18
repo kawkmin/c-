e46f6b6b17f87f84e153a12aac3c026b93f22229
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-18 20:31:30 +0900

    Create 가장 큰 수(LV2).cpp

---
+++ 프로그래머스/가장 큰 수(LV2).cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+bool cmp(string s, string s2) {
+    return s + s2 > s2 + s;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<string> v;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        v.push_back(to_string(numbers[i]));
+    }
+    sort(v.begin(), v.end(), cmp);
+    if (v[0] == "0") return "0";
+    for (int i = 0; i < v.size(); i++)
+        answer += v[i];
+
+    return answer;
+}
