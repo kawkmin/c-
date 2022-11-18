a72d197d37dd693fd27ce757c04677ed7e76028c
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-18 20:31:29 +0900

    Create 문자열 압축(LV2) 더럽게 품.cpp

---
+++ 프로그래머스/문자열 압축(LV2) 더럽게 품.cpp
@@ -0,0 +1,52 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+#include<algorithm>
+using namespace std;
+
+int solution(string s) {
+    int answer = 1001;
+    for (int i = 1; i <= s.length() / 2; i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < s.length(); j += i)
+        {
+            string temp = s.substr(j, i);
+            cnt += temp.length();
+            bool b = false;
+            int cnt2 = 1;
+            int x = 10;
+            for (int k = j + i; k < s.length(); k += i)
+            {
+                string temp2 = s.substr(k, i);
+                if (temp == temp2)
+                {
+                    j += i;
+                    if (!b)
+                    {
+                        b = true;
+                        cnt++;
+                        cnt2++;
+                    }
+                    else
+                    {
+                        cnt2++;
+                        if (cnt2 == x)
+                        {
+                            cnt++;
+                            x *= 10;
+                        }
+                        continue;
+                    }
+                }
+                else
+                    break;
+            }
+        }
+        answer = min(answer, cnt);
+
+    }
+    if (answer == 1001)
+        return s.length();
+    return answer;
+}
