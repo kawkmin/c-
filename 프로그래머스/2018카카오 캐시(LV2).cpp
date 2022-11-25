8994a868f8dc9cb622ac9b477491d2dde4188589
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-25 17:46:44 +0900

    Create 2018카카오 캐시(LV2).cpp

---
+++ 프로그래머스/2018카카오 캐시(LV2).cpp
@@ -0,0 +1,35 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities) {
+    if (cacheSize == 0)
+        return cities.size() * 5;
+
+    int answer = 0;
+    vector<string> v;
+    for (int i = 0; i < cities.size(); i++)
+    {
+        for (int j = 0; j < cities[i].size(); j++)
+        {
+            if (cities[i][j] < 'a')
+                cities[i][j] += 'a' - 'A';
+        }
+        int cnt = 5;
+        v.push_back(cities[i]);
+        for (int j = 0; j < v.size()-1; j++)
+        {
+            if (v[j] == cities[i])
+            {
+                v.erase(v.begin() + j);
+                cnt = 1;
+                break;
+            }
+        }
+        if (v.size() > cacheSize)
+            v.erase(v.begin());
+        answer += cnt;
+    }
+    return answer;
+}
