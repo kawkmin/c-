08da22bff939e955abd7e7b084a2200b67828398
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-26 18:44:02 +0900

    Create 타겟넘버(LV2).cpp

---
+++ 프로그래머스/타겟넘버(LV2).cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include<algorithm>
+using namespace std;
+int visit[21] = { 0, };
+int answer = 0;
+int t;
+
+vector<int> v;
+void dfs(int cnt, int dep)
+{
+    if (dep == v.size())
+    {
+        if (cnt == t)
+        {
+            answer++;
+        }
+        return;
+    }
+    dfs(cnt + v[dep], dep + 1);
+    dfs(cnt - v[dep], dep + 1);
+}
+
+int solution(vector<int> numbers, int target) {
+    v = numbers;
+    t = target;
+    dfs(0, 0);
+    return answer;
+}
