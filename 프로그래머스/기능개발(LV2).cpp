fcbfba5c8f6c7b2c121e948a84633023ef95a168
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-27 17:50:22 +0900

    Create 기능개발(LV2).cpp

---
+++ 프로그래머스/기능개발(LV2).cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+	vector<int> answer;
+	vector<int> v = progresses;
+	while (v.size() != 0)
+	{
+		for (int i = 0; i < v.size(); i++) {
+			if (v[i] >= 100)
+				continue;
+			v[i] += speeds[i];
+		}
+		int cnt = 0;
+		int size = v.size();
+		for (int i = 0; i < size; i++)
+		{
+			if (v.front() >= 100)
+			{
+				cnt++;
+				speeds.erase(speeds.begin());
+				v.erase(v.begin());
+				continue;
+			}
+			break;
+		}
+		if (cnt > 0)
+			answer.push_back(cnt);
+	}
+
+	return answer;
+}
