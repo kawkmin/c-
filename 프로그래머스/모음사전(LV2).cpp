74734987792e8a23016cf9050e3e2bd958390007
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-27 17:50:23 +0900

    Create 모음사전(LV2).cpp

---
+++ 프로그래머스/모음사전(LV2).cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include<map>
+using namespace std;
+char arr[5] = { 'A','E','I','O','U' };
+int place = 1;
+map<string, int> m;
+void dfs(string str, int dep)
+{
+    if (dep == 5)
+        return;
+    for (int i = 0; i < 5; i++)
+    {
+        string s = str;
+        s += arr[i];
+        m[s] = place++;
+        dfs(s, dep + 1);
+    }
+}
+
+int solution(string word) 
+{
+    dfs("", 0);   
+    return m[word];
+}
