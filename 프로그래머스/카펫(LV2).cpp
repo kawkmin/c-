2a855d609896172d53653b5f8106586838c7647e
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-28 15:12:39 +0900

    Create 카펫(LV2).cpp

---
+++ 프로그래머스/카펫(LV2).cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    for (int i = 1; i <= brown; i++)
+    {
+        for (int j = 1; j <= brown; j++)
+        {
+            if ((i - 2) * (j - 2) == yellow)
+            {
+                if ((i * 2) + (j * 2) - 4 == brown)
+                {
+                    answer.push_back(j);
+                    answer.push_back(i);
+                    return answer;
+                }
+            }
+        }
+    }
+    return answer;
+}
