c0d0436ddb149b9e4729c6cb8d95d044b1f544d8
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-30 23:21:57 +0900

    Create 행렬의 곱셈(LV2).cpp

---
+++ 프로그래머스/행렬의 곱셈(LV2).cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    for (int i = 0; i < arr1.size(); i++)
+    {
+        vector<int> v;
+        for (int j = 0; j < arr2[0].size(); j++)
+        {
+            int x = 0;
+            for (int k = 0; k < arr2.size(); k++)
+            {
+                x += arr1[i][k] * arr2[k][j];
+            }
+            v.push_back(x);
+        }
+        answer.push_back(v);
+    }
+    return answer;
+}
