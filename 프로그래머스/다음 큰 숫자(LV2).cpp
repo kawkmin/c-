a723cc9a431a5587d6bd93a8c136d8ca45ae59d3
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-30 23:22:00 +0900

    Create 다음 큰 숫자(LV2).cpp

---
+++ 프로그래머스/다음 큰 숫자(LV2).cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = n;
+    int next_n = 0;
+    int cntx = 0;
+    while (n > 0)
+    {
+        if (n % 2 == 1)
+            cntx++;
+        n /= 2;
+    }
+    while (true)
+    {
+        next_n = answer + 1;
+        answer = next_n;
+        int cnty = 0;
+        while (next_n > 0)
+        {
+            if (next_n % 2 == 1)
+                cnty++;
+            next_n /= 2;
+        }
+        if (cnty == cntx)
+            return answer;
+    }
+}
