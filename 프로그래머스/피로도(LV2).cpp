f905eba50bd046552705c5c8a25fbfc2b560e29b
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-03 20:28:07 +0900

    Create 피로도(LV2).cpp

---
+++ 프로그래머스/피로도(LV2).cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+vector<vector<int>> v;
+int visit[8] = { 0, };
+int answer = 0;
+
+void dfs(int tire, int cnt)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (!visit[i])
+        {
+            if (tire >= v[i][0])
+            {
+                visit[i] = 1;
+                dfs(tire - v[i][1], cnt + 1);
+                visit[i] = 0;
+            }
+        }
+    }
+    answer = max(answer, cnt);
+}
+
+int solution(int k, vector<vector<int>> dungeons) {
+    v = dungeons;
+    dfs(k, 0);
+    return answer;
+}
