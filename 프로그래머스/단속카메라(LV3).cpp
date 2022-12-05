923ca1b3456cd9f72f151205c5254899e12ff2fb
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-05 18:07:45 +0900

    Create 단속카메라(LV3).cpp

---
+++ 프로그래머스/단속카메라(LV3).cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+bool cmp(vector<int> v1, vector<int> v2)
+{
+    return v1[1] < v2[1];
+}
+
+int solution(vector<vector<int>> routes) {
+    int answer = 0;
+    sort(routes.begin(), routes.end(), cmp);
+    int temp = -30001;
+    for (int i = 0; i < routes.size(); i++)
+    {
+        if (temp < routes[i][0])
+        {
+            answer++;
+            temp = routes[i][1];
+        }
+    }
+    return answer;
+}
