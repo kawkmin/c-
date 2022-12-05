53cdf21dfb292e4836286b716eb817c7a47b3cf4
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-05 18:07:48 +0900

    Create 멀쩡한 사각형(LV2)틀림.cpp

---
+++ 프로그래머스/멀쩡한 사각형(LV2)틀림.cpp
@@ -0,0 +1,19 @@
+﻿using namespace std;
+
+int gcd(int a, int b) {
+    int c;
+    while (b != 0) {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
+}
+
+long long solution(int w, int h) {
+    long long W = w;
+    long long H = h;
+    long long answer = (W * H) - (w + h - gcd(w, h));
+
+    return answer;
+}
