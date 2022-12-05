045440ea105a318f683a158a579b9747c2e3d4a1
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-05 18:07:29 +0900

    Create 유클리드 호제법(최대 공약수,최소 공배수).cpp

---
+++ 알고리즘 기초/유클리드 호제법(최대 공약수,최소 공배수).cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+int gcd(int a, int b) {
+	int r;
+	while (b != 0) {
+		r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << "두 수의 최대공약수는 : " << gcd(a, b) << " 입니다" << endl;
+	cout << "두 수의 최소공배수는 : " << (a * b) / gcd(a, b) << " 입니다" << endl;
+}
