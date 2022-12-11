434a689e9af3364f8cd1cc1c9e556f7c1e13f7c2
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-11 21:26:25 +0900

    10분안에 풀림

---
+++ 프로그래머스/여행경로(LV3).cpp
@@ -0,0 +1,37 @@
+﻿#include <string>
+#include <vector>
+#include<algorithm>
+using namespace std;
+int visit[10001] = { 0, };
+vector<vector<string>> t;
+vector<vector<string>> an;
+vector<string> v;
+
+void dfs(string s, int dep)
+{
+    if (dep == t.size())
+    {
+        an.push_back(v);
+    }
+
+    for (int i = 0; i < t.size(); i++)
+    {
+        if (t[i][0] == s && !visit[i])
+        {
+            visit[i] = 1;
+            v.push_back(t[i][1]);
+            dfs(t[i][1], dep + 1);
+            v.pop_back();
+            visit[i] = 0;
+        }
+    }
+}
+
+vector<string> solution(vector<vector<string>> tickets) {
+    vector<string> answer;
+    t = tickets;
+    v.push_back("ICN");
+    dfs("ICN", 0);
+    sort(an.begin(), an.end());
+    return an[0];
+}
