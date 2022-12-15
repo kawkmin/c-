079e760c06e419762f888b9452f19918a41cea63
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-15 19:17:18 +0900

    Create 점 찍기(LV2).cpp

---
+++ 프로그래머스/점 찍기(LV2).cpp
@@ -0,0 +1,14 @@
+﻿#include <string>
+#include <vector>
+#include<cmath>
+using namespace std;
+
+long long solution(int k, int d) {
+    long long answer = 0;
+    for (int i = 0; i <= d; i += k)
+    {
+        long long y = sqrt(pow(d, 2) - pow(i, 2));
+        answer += (y / k) + 1;
+    }
+    return answer;
+}
