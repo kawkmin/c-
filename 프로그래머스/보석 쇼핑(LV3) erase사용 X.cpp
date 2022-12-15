08032d52e805f30eb7c5f1316d6294f10d902592
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-15 16:12:44 +0900

    Create 보석 쇼핑(LV3) erase사용 X.cpp

---
+++ 프로그래머스/보석 쇼핑(LV3) erase사용 X.cpp
@@ -0,0 +1,46 @@
+﻿#include <string>
+#include <vector>
+#include<unordered_map>
+#include<unordered_set>
+#include<algorithm>
+using namespace std;
+
+vector<int> solution(vector<string> gems) {
+
+    vector<int> answer;
+    unordered_map<string, int> m;
+    unordered_set<string> s;
+    for (int i = 0; i < gems.size(); i++)
+    {
+        s.insert(gems[i]);
+    }
+    int x = 0;
+    int MIN = 123456;
+    for (int i = 0; i < gems.size(); i++)
+    {
+        m[gems[i]]++;
+        
+        if (m.size() == s.size())
+        {
+            while (m[gems[x]] > 1) 
+            {
+                m[gems[x]]--;
+                x++;
+            }
+
+            if (MIN > i - x)
+            {
+                MIN = i - x;
+                answer = { x+1,i + 1 };
+            }
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<string> v = { "DIA", "EM", "EM", "RUB", "DIA" };
+    vector<int> v2 = solution(v);
+}
