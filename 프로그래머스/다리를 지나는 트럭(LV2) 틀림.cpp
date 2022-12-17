a4ffcc2d8bf6f0c7a9f907fe06085ef137ed416f
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-17 23:48:37 +0900

    Create 다리를 지나는 트럭(LV2) 틀림.cpp

---
+++ 프로그래머스/다리를 지나는 트럭(LV2) 틀림.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <vector>
+#include<queue>
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 0;
+    queue<int> q;
+    int total_weight = 0;
+    int index = 0;
+
+    while (1)
+    {
+        if (index == truck_weights.size())
+        {
+            answer += bridge_length;
+            break;
+        }
+        answer++;
+
+        if (q.size() == bridge_length)
+        {
+            total_weight -= q.front();
+            q.pop();
+        }
+
+        if (truck_weights[index] + total_weight <= weight)
+        {
+            q.push(truck_weights[index]);
+            total_weight += truck_weights[index];
+            index++;
+        }
+        else
+        {
+            q.push(0);
+        }
+    }
+
+    return answer;
+}
