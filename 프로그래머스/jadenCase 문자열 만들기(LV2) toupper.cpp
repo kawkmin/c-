0df73fc42923863c1f12cf9655859e089a59cb0d
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-19 17:46:56 +0900

    Create jadenCase 문자열 만들기(LV2) toupper.cpp

---
+++ 프로그래머스/jadenCase 문자열 만들기(LV2) toupper.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+//toupper(char c) = 대문자로만 반환 tolower
+
+string solution(string s) {
+    string answer = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (i == 0 || s[i - 1] == ' ')
+        {
+            if (s[i] >= 'a' && s[i] <= 'z')
+                answer += s[i] + 'A' - 'a';
+            else
+                answer += s[i];
+        }
+        else
+        {
+            if (s[i] >= 'A' && s[i] <= 'Z')
+                answer += s[i] - ('A' - 'a');
+            else
+                answer += s[i];
+        }
+    }
+    return answer;
+}
