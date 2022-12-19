03c01d066315034ae14721d092e2f5df86838ac9
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-19 17:46:46 +0900

    Create 숫자표현(LV2).cpp

---
+++ 프로그래머스/숫자표현(LV2).cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int x = 0;
+        for (int j = i; j <= n; j++)
+        {
+            x += j;
+            if (x > n)
+                break;
+            if (x == n) {
+                answer++;
+                break;
+            }
+        }
+    }
+    return answer;
+}
